4da2e419145a0eded7a014230870792b22a08afa
mbammy <kschamplin19@blakeschool.org>
2015-11-19 12:34:00 -0600

    fix for newer node

--- modules/dataprocessor.js
+++ modules/dataprocessor.js
@@ -3,40 +3,34 @@
 var ioredis = require('ioredis'); // redis clients
 var moment = require('moment'); //date/time thing
 var scheduler = require('node-schedule'); // autoupdater
-var _ = require('underscore'); // hurray we know what this means
+var underscore = require('underscore'); // hurray we know what this means
 // Connect to the redis server
 var redis = new ioredis(6379, 'jspamc.homelinux.com');
-redis.on('error', function(err) {
-  throw err;
-});
+redis.on('error', function(err) {throw err;});
 var redislistener = new ioredis(6379, 'jspamc.homelinux.com');
 redislistener.on('error', function(err) {
   throw err;
 });
 redislistener.subscribe('dataprocessor');
 // Define functions
-var today = [],
+var today,
   currentClass = [],
   nextClass = [],
   remainingTime = [];
 var gettoday = function() {
   redis.get('today', function(err, data) {
-    if (err) throw err;
-    if (Array.isArray(data)) {
+      if (err) throw err;
       today = JSON.parse(data);
-    } else {
-      today = "No School";
-    }
-  });
+      // today = "No School";
+    });
 };
 gettoday(); // We should call this ASAP
 var isnow = function() { // determine current class
   if (today !== "No School") {
-    currentClass = _.filter(today, function(item) {
-      return moment().isBetween(moment().hour(item.shour).minute(item.smin),
-        moment().hour(item.ehour).minute(item.smin), 'seconds');
+    currentClass = underscore.filter(today, function(item) {
+      return moment().isBetween(moment({h:item.shour, m: item.smin}), moment({h:item.ehour, m: item.emin}));
     });
-    if (Array.isArray(currentClass)) redis.set('currentclass', JSON.stringify(currentClass));
+    if (currentClass.length >= 1) redis.set('currentclass', JSON.stringify(currentClass));
     else redis.set('currentclass', "No School");
   } else {
     redis.set('currentclass', "No School");
@@ -45,8 +39,8 @@ var isnow = function() { // determine current class
 
 var isnext = function() {
   if (today !== "No School") {
-    var upcoming = _.filter(today, function(item) {
-      return moment().isBefore(moment().hour(item.shour).minute(item.smin));
+    var upcoming = underscore.filter(today, function(item) {
+      return moment().isBefore(moment({h:item.shour}));
     });
     nextClass = upcoming.slice(0, currentClass.length);
     if (nextClass.length >= 1) redis.set('nextclass', JSON.stringify(nextClass));
@@ -58,11 +52,11 @@ var isnext = function() {
 
 var endsin = function() {
   if (today !== "No School" && currentClass !== "No School") {
-    _.each(currentClass, function(item) { // do for all in currentclass
-      item.etime = moment().hour(item.ehour).minute(item.emin); // add etime to item
+    underscore.each(currentClass, function(item) { // do for all in currentclass
+      item.etime = moment({h:item.ehour, m:item.emin}); // add etime to item
       item.remainingtime = Math.floor(moment.duration(item.etime.diff(moment())).asMinutes()); //add remainingtime to item
     });
-    remainingTime = _.pluck(currentClass, 'remainingtime'); //get remainingtime from all entries in currentClass
+    remainingTime = underscore.pluck(currentClass, 'remainingtime'); //get remainingtime from all entries in currentClass
     redis.set('remainingtime', JSON.stringify(remainingTime)); // set it
   } else {
     redis.set('remainingtime', "No School");
