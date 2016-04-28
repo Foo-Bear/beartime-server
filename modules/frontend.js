07fe2fb6c374cd4a1945e910a9a341b1ab8cc0b8
Saji Champlin <kschamplin19@blakeschool.org>
2016-04-28 09:40:02 -0500

    temp disable ssl

--- modules/frontend.js
+++ modules/frontend.js
@@ -9,9 +9,9 @@ var app = express()
 // var underscore = require('underscore')
 var morgan = require('morgan')
 var config = require('../config.js')
-var fs = require('fs')
+// var fs = require('fs')
 var ecstatic = require('ecstatic')
-var https = require('https')
+// var https = require('https')
 app.use(morgan('dev'))
 
 console.log('Booting Frontend')
@@ -22,23 +22,30 @@ config.connect('frontend')
  * Important Note
  * Because we are using redis as a backend, it costs almost nothing to patch each request directly through.
  * This reduces load in the long term because the frontend needs no memory or update scheme.
- */
+*/
 
-// CORS hedaders
+// CORS headers
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
+
+/*
 var privateKey = fs.readFileSync('../sslcert/server.key').toString()
 var certificate = fs.readFileSync('../sslcert/server.crt').toString()
-
 var credentials = {key: privateKey, cert: certificate}
+*/
+
 app.use('/api', require('./routes/schedules')) // load the schedules routes
 app.use('/api', require('./routes/authentication')) // load the schedules routes
 app.use('/', ecstatic({ root: config.webroot }))
+
+/*
 var httpsServer = https.createServer(credentials, app)
 httpsServer.listen(3000)
+*/
+
 console.log('Reporting to service set')
 redis.zincrby('services', 1, 'frontend') // add us to the list
 
